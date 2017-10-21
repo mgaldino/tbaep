d46c5b71eaae4fccac50eba2a189106e396315e1
mgaldino <mcz.fea@gmail.com>
2017-10-21 10:48:29 -0200

    Acrescentei breve documentação

--- R/recebe_input.R
+++ R/recebe_input.R
@@ -1,7 +1,11 @@
-## função para receber input, em prompt, do usuário
-## pergunta se quer imprimir todos os arquivos modificados
+#' This function gets input from user
+#'
+#' @keywords prompt, input
+#' @export
+#' @examples
+#' recebe_input()
 
 recebe_input <- function(){
-  x <- readline("Você quer imprimir os nomes de todos dos arquivos sem acento? responda s ou n: ")
+  x <- readline("Você quer imprimir os nomes de todos os arquivos sem acento? responda s ou n: ")
   x <- unlist(strsplit(x, ","))
 }
